8556998345cfb24a4c51d8afbf186797c18bf8e1
Masato Hatayama <masato.hatayama@gmail.com>
2019-10-26 06:29:57 +0900

    complete

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,31 +1,65 @@
+export class Node {
+  constructor(value, prev, next) {
+    this.value = value
+    this.prev = prev
+    this.next = next
+  }
+}
+
 export class LinkedList {
   constructor() {
-    this.container = []
+    this.first = null
+    this.last = null
   }
 
   push(value) {
-    this.container.push(value)
+    const pushNode = new Node(value, this.last, null)
+    if (this.last) this.last.next = pushNode
+    if (!this.first) this.first = pushNode
+    this.last = pushNode
   }
 
   pop() {
-    return this.container.pop()
+    const popNode = this.last
+    this.last = this.last.prev
+    if (popNode == this.first) this.first = null
+    return popNode.value
   }
 
   shift() {
-    return this.container.shift()
+    const shiftNode = this.first
+    this.first = this.first.next
+    if (shiftNode == this.last) this.last = null
+    return shiftNode.value
   }
 
   unshift(value) {
-    this.container.unshift(value)
+    const unshiftNode = new Node(value, null, this.first)
+    if (this.first) this.first.prev = unshiftNode
+    if (!this.last) this.last = unshiftNode
+    this.first = unshiftNode
   }
 
   delete(value) {
-    let idx = this.container.indexOf(value)
-    if (idx >= 0)
-      this.container.splice(idx, 1)
+    let currentNode = this.first
+    do {
+      if (currentNode.value === value) {
+        if (currentNode.prev) currentNode.prev.next = currentNode.next
+        if (currentNode.next) currentNode.next.prev = currentNode.prev
+        if (this.first == currentNode) this.first = this.first.next
+        if (this.last == currentNode) this.last = this.last.prev
+        return
+      }
+    } while (currentNode = currentNode.next)
   }
 
   count() {
-    return this.container.length
+    let cnt = 0
+    let currentNode = this.first
+    while (currentNode) {
+      cnt += 1
+      currentNode = currentNode.next
+    }
+    return cnt
   }
 }
